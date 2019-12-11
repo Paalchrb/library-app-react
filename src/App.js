3c39418a6a46fa4aecb8c9961c67bbdc959e5403
Pål-Christian By <Pal.christian.by@gmail.com>
2019-12-11 17:02:14 +0100

    added header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-add();
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Header 
+          title={'Dr. BomBy'}
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    const { title } = this.props;
+    return (
+      <h1>{title}</h1>
+    )
+  }
+}
+
+export default Header;
