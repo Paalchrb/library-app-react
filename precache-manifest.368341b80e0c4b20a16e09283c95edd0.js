f88f4f81e48594faeba410cad2bb910674367bac
Pål-Christian By <Pal.christian.by@gmail.com>
2019-12-11 17:02:43 +0100

    Updates

---
+++ precache-manifest.368341b80e0c4b20a16e09283c95edd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e67fd0108283ce6cacede3afdb009e",
+    "url": "/library-app-react/index.html"
+  },
+  {
+    "revision": "5d8f07673edf20cb6713",
+    "url": "/library-app-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d7465656765b1a317532",
+    "url": "/library-app-react/static/js/2.6e5c9a82.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/library-app-react/static/js/2.6e5c9a82.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d8f07673edf20cb6713",
+    "url": "/library-app-react/static/js/main.9f4b784d.chunk.js"
+  },
+  {
+    "revision": "3c4fdf25cf5bbef2621c",
+    "url": "/library-app-react/static/js/runtime-main.24afc473.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/library-app-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
